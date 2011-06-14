48401b7702c50366d27adefd6ddeb59feca1996f
Eric Hopper <hopper@omnifarious.org>
2011-06-14 13:37:30 -0700

    Simple user-defined functions.

--- forthlike.py
+++ forthlike.py
@@ -2,20 +2,22 @@ import sys, shlex, readline, os, string
 List, assign, call, add, sub, div, Pow, mul, mod, fac, duf, read,\
 kill, clr, STO, RET, fib, curs = {}, "set", "get", "+", "-", "/", "^", "*",\
 "%", "fact", "func", "read", "kill", "clear", ">", "@", "fib", "vars"
+funcdict = {}
 def fact(num):
     if num == 1: return 1
     else: return num*fact(num-1)
 def builtin_op(op, stack):
     global List
+    global funcdict
     if op == mul: stack.append(float(stack.pop())*float(stack.pop()))
     elif op == div: stack.append(float(stack.pop())/float(stack.pop()))
     elif op == sub: stack.append(float(stack.pop())-float(stack.pop()))
     elif op == add: stack.append(float(stack.pop())+float(stack.pop()))
     elif op == Pow: stack.append(float(stack.pop())**float(stack.pop()))
     elif op == assign: val = List[stack.pop()] = stack.pop(); stack.append(val)
-    elif op == call: stack.append(List[stack.pop()])
+    elif op == call: Eval(funcdict[stack.pop()], stack)
     elif op == fac: stack.append(fact(stack.pop()))
-    elif op == duf: stack.append("%s %s %s" % (duf, stack.pop(), stack.pop()))
+    elif op == duf: name = stack.pop(); funcdict[name] = stack.pop(); stack.append(name)
     elif op == mod: stack.append(float(stack.pop())%float(stack.pop()))
     elif op == kill: del List[stack.pop()]
     elif op == clr: os.system("clear")
@@ -23,9 +25,11 @@ def builtin_op(op, stack):
     elif op == RET: stack.append(List[stack.pop()])
     elif op == curs: stack.append(List)
     elif op == read: prompt = stack.pop(); List[prompt] = Eval(raw_input("%s "%prompt)); stack.append(List[prompt])
-def Eval(expr):
+def Eval(expr, stack=None):
     ops = "%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s"%(mul, add, sub, div, Pow, assign, call, fac, duf, mod, read, kill, clr, STO, RET, curs)
-    stack, expr, ops = [], shlex.split(string.lower(expr)), ops.split()
+    if stack is None:
+        stack = []
+    expr, ops = shlex.split(string.lower(expr)), ops.split()
     for i in expr:
         if i[0] != ';':
             if i not in ops: stack.append(i)
